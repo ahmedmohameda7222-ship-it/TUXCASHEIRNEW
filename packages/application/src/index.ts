e25731821ef99af75e43ba67d04e15b1b2f4d2d9
ahmedmohameda7222-ship-it <ahmed.mohameda7222@gmail.com>
2026-08-17 22:38:36 +0200

    Export operator session application API

--- packages/application/src/index.ts
+++ packages/application/src/index.ts
@@ -1,3 +1,11 @@
 export type { ApplicationError, ApplicationErrorCode } from './errors';
 export { err, ok } from './result';
 export type { Result } from './result';
+export { greetingForHour, OperationsSessionService } from './session';
+export type {
+  OperationsSessionResult,
+  OperationsSessionState,
+  OperatorSummary,
+  PinVerifier,
+  SessionRuntime,
+} from './session';
