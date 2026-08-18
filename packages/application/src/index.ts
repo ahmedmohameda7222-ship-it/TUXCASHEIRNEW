203d396408eb70fada575a97a09afac50b067685
ahmedmohameda7222-ship-it <ahmed.mohameda7222@gmail.com>
2026-08-18 10:28:09 +0200

    Export Orders application service

--- packages/application/src/index.ts
+++ packages/application/src/index.ts
@@ -1,4 +1,17 @@
+export { ApplicationCommandCoordinator } from './commandCoordinator';
 export type { ApplicationError, ApplicationErrorCode } from './errors';
+export {
+  createEmptyOrderDraft,
+  OperationsOrdersService,
+} from './orders';
+export type {
+  OrderPlacement,
+  OrderPlacementError,
+  OrderPlacementResult,
+  OrdersRuntime,
+  OrdersWorkspace,
+  OrdersWorkspaceResult,
+} from './orders';
 export { err, ok } from './result';
 export type { Result } from './result';
 export { greetingForHour, OperationsSessionService } from './session';
