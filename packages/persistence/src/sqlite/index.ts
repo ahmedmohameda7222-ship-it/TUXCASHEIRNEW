a5b2ee98c9a4e8dc6613fdd03edf9413fe71369c
ahmedmohameda7222-ship-it <ahmed.mohameda7222@gmail.com>
2026-08-18 15:08:31 +0200

    Phase 7: export SQLite Bulk Stock store

--- packages/persistence/src/sqlite/index.ts
+++ packages/persistence/src/sqlite/index.ts
@@ -1,4 +1,5 @@
 export { SQLITE_MIGRATIONS, applySqliteMigrations, type SqliteMigration } from './migrations';
+export { SqliteBulkStockStore } from './SqliteBulkStockStore';
 export { SqliteExpenseLedgerStore } from './SqliteExpenseLedgerStore';
 export { SqliteOperationsDatabase } from './SafeSqliteOperationsDatabase';
 export { SqliteOperatorSessionReadModel } from './SqliteOperatorSessionReadModel';
