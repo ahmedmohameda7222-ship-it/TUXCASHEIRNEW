29bea4eb05924c45333e08d928b7ac9f68712826
ahmedmohameda7222-ship-it <ahmed.mohameda7222@gmail.com>
2026-08-18 10:21:16 +0200

    Export order draft store contract

--- packages/persistence/src/index.ts
+++ packages/persistence/src/index.ts
@@ -1,2 +1,3 @@
 export type * from './contracts';
 export type { OperatorSessionReadModel } from './operatorSessionReadModel';
+export type { OrderDraftKey, OrderDraftStore } from './orderDraftStore';
