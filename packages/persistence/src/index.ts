e0ce6aeaea9d796169c1077650b9d3389489edaa
ahmedmohameda7222-ship-it <ahmed.mohameda7222@gmail.com>
2026-08-25 04:53:46 +0200

    feat: export worker UI preference repository contract

--- packages/persistence/src/index.ts
+++ packages/persistence/src/index.ts
@@ -3,3 +3,4 @@ export type * from './contracts';
 export type { ExpenseLedgerMutation, ExpenseLedgerStore } from './expenseLedgerStore';
 export type { OperatorSessionReadModel } from './operatorSessionReadModel';
 export type { OrderDraftKey, OrderDraftStore } from './orderDraftStore';
+export type { WorkerUiPreferencesRepository } from './workerUiPreferencesStore';
