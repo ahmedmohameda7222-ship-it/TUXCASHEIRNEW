806cc70ce7aa3ae712a1224a3119acb70b1b3638
ahmedmohameda7222-ship-it <ahmed.mohameda7222@gmail.com>
2026-08-29 11:46:41 +0200

    style: normalize concurrency test formatting

--- packages/application/src/workerUiPreferencesConcurrency.test.ts
+++ packages/application/src/workerUiPreferencesConcurrency.test.ts
@@ -18,12 +18,8 @@ import {
 
 const shopId = parseEntityId<ShopId>('11111111-1111-4111-8111-111111111111');
 const workerId = parseEntityId<WorkerId>('22222222-2222-4222-8222-222222222222');
-const categoryA = parseEntityId<MenuCategoryId>(
-  '33333333-3333-4333-8333-333333333331',
-);
-const categoryB = parseEntityId<MenuCategoryId>(
-  '33333333-3333-4333-8333-333333333332',
-);
+const categoryA = parseEntityId<MenuCategoryId>('33333333-3333-4333-8333-333333333331');
+const categoryB = parseEntityId<MenuCategoryId>('33333333-3333-4333-8333-333333333332');
 const productA = parseEntityId<ProductId>('44444444-4444-4444-8444-444444444441');
 const productB = parseEntityId<ProductId>('44444444-4444-4444-8444-444444444442');
 const accentA = parseSystemAccentColor('#1E3A8A');
